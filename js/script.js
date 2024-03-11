3887be68318a0f4e76561c99f8dc197975fcf775
Azamat Zhamankulov <azamat.zhamankulov.kk@gmail.com>
2024-03-11 15:49:20 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+"use strict"
+
+// lesson # 13 comunication with user
+
+// const answers = [];
+
+// // answers[0] = prompt('What is your name?', '');
+// // answers[1] = prompt('What is your family name?', '');
+// // answers[2] = prompt('How old are you?', '');
+
+// console.log(typeof(answers));
+// console.log(answers);
+// document.write(answers);
+
+
+// lesson # 14 Interpolation
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const username = "Ivan";
+// alert(`Guten Morgen, ${username}`);
+
+
+// lesson # 15 Operators in JavaScript
+// let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--;
+ 
+// console.log(++incr);
+// console.log(--decr);
+
+
+// lesson # 16 System GIT
+
