e19415867071991d8bcefdfb51a1183774ab54fd
Azamat Zhamankulov <azamat.zhamankulov.kk@gmail.com>
2024-05-12 18:19:03 +0600

    Added solution from documantation

--- js/script.js
+++ js/script.js
@@ -2253,3 +2253,33 @@
 // const str = "11 - 30    * 4 ARAFSFSfdsf      fsf JavaScript";
 // const parts = str.split(/\s+/).filter(symbol => symbol.trim() !== '');
 // console.log(parts);
+
+//! Calculator with split method 
+
+// function Calculator() {
+
+//     this.methods = {
+//       "-": (a, b) => a - b,
+//       "+": (a, b) => a + b
+//     };
+  
+//     this.calculate = function(str) {
+  
+//       let split = str.split(' '),
+//         a = +split[0],
+//         op = split[1],
+//         b = +split[2];
+  
+//       if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+//         return NaN;
+//       }
+  
+//       return this.methods[op](a, b);
+//     };
+  
+//     this.addMethod = function(name, func) {
+//       this.methods[name] = func;
+//     };
+//   }
+//   let calc = new Calculator;
+//   console.log(calc.calculate("3 + 2"));
