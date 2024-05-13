663cbb507502179f18f74b3e08195a1c9a0ee25c
Azamat Zhamankulov <azamat.zhamankulov.kk@gmail.com>
2024-05-13 18:08:34 +0600

    Added next exercise for mapping objects

--- js/script.js
+++ js/script.js
@@ -2306,4 +2306,16 @@
 // console.log(names);
 
 
+//! Map to objects
+//todo You have an array of user objects, each one has name, surname and id. Write the
+//todo code to create another array from it, of objects with id and fullName, where
+//todo fullName is generated from name and surname. So, actually you need to map one
+//todo array of objects to another. Try using => here. There’s a small catch.
 
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+// let usersMapped = users.map((item) => )
