07787f21c9700456d4c9b97c1b7d088776b36c6b
Azamat Zhamankulov <azamat.zhamankulov.kk@gmail.com>
2024-05-13 13:02:40 +0600

    Added mapping an array with arr.map((item) => item.name);

--- js/script.js
+++ js/script.js
@@ -2286,3 +2286,24 @@
 //   }
 //   let calc = new Calculator;
 //   console.log(calc.calculate("3 ** 2"));
+
+
+//! Map to names
+//todo You have an array of user objects, each one has user.name. Write the code that
+//todo converts it into an array of names.
+
+// let john = { name: "John",
+//              age: 25 };
+// let pete = { name: "Pete",
+//              age: 30 };
+// let mary = { name: "Mary",
+//              age: 28 };
+
+// let users = [ john, pete, mary ];
+
+// let names = users.map((item) => item.name);
+
+// console.log(names);
+
+
+
