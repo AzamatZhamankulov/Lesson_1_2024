13852ca991d8874766a7f7774b14f0b6d05f4206
Azamat Zhamankulov <azamat.zhamankulov.kk@gmail.com>
2024-05-28 22:32:17 +0600

    Added RECURSION function with edited data

--- js/script.js
+++ js/script.js
@@ -3027,17 +3027,130 @@
 // console.log(givePower(2, 2)); //4
 
 //todo NOW how it can be if we use RECURSION
-function pow(x, n) {
-  if (n === 1) {
-    return x;
+// function pow(x, n) {
+//   if (n === 1) {
+//     return x;
+//   } else {
+//     return x * pow (x, n -1);
+//   }
+// }
+
+// console.log(pow(2, 1)) //2
+// console.log(pow(2, 2)) //4
+// console.log(pow(2, 3)) //8
+// console.log(pow(2, 4)) //16
+// console.log(pow(3, 3)) //27
+// console.log(pow(9, 3)) //729
+
+//todo let's see with example
+// let students = {
+//   js: [{
+//     name: 'John',
+//     progress: 100
+//   }, {
+//     name: 'Ivan',
+//     progress: 60
+//   }],
+
+//   html: {
+//     basic: [{
+//       name: 'Peter',
+//       progress: 20
+//     }, {
+//       name: 'Ann',
+//       progress: 18
+//     }],
+
+//     pro: [{
+//       name: 'Sam',
+//       progress: 10
+//     }]
+//   }
+// };
+
+// function getTotalProgressByItteration(data) {
+//   let total = 0;
+//   let students = 0;
+
+//   for (let course of Object.values(data)) {
+//     if (Array.isArray(course)) {
+//       students += course.length;
+
+//       for (let i = 0; i < course.length; i++) {
+//         total += course[i].progress;
+//       }
+//     } else {
+//       for (let subCourse of Object.values(course)) {
+//         students += subCourse.length;
+
+//         for (let i = 0; i < subCourse.length; i++) {
+//           total += subCourse[i].progress;
+//         }
+//       }
+//     }
+//   }
+
+//   return total / students
+// }
+
+// console.log(getTotalProgressByItteration(students)); // 41.6
+
+
+//! what if we will have edited data with extra courses
+
+let students = {
+  js: [{
+    name: 'John',
+    progress: 100
+  }, {
+    name: 'Ivan',
+    progress: 60
+  }],
+
+  html: {
+    basic: [{
+      name: 'Peter',
+      progress: 20
+    }, {
+      name: 'Ann',
+      progress: 18
+    }],
+
+    pro: [{
+      name: 'Sam',
+      progress: 10
+    }],
+
+    someEditionalData: {
+      students: [{
+        name: 'Test',
+        progress: 100
+      }]
+    }
+  }
+};
+
+function getTotalProgressByRecoursion(data) {
+  if (Array.isArray(data)) {
+    let total = 0;
+
+    for (let i = 0; i < data.length; i++) {
+      total += data[i].progress;
+    }
+    return [total, data.length];
   } else {
-    return x * pow (x, n -1);
+    let total = [0 , 0];
+
+    for (let subData of Object.values(data)) {
+      const subDataArray = getTotalProgressByRecoursion(subData);
+      total[0] += subDataArray[0];
+      total[1] += subDataArray[1];
+    }
+
+    return total;
   }
 }
 
-console.log(pow(2, 1)) //2
-console.log(pow(2, 2)) //4
-console.log(pow(2, 3)) //8
-console.log(pow(2, 4)) //16
-console.log(pow(3, 3)) //27
-console.log(pow(9, 3)) //729
+const result = getTotalProgressByRecoursion(students);
+
+console.log(result[0]/result[1]); // 51.333
