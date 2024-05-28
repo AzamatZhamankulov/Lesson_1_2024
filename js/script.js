7b6d35dccd031da15c24f7aa94620853e7f36103
Azamat Zhamankulov <azamat.zhamankulov.kk@gmail.com>
2024-05-28 21:44:08 +0600

    Added begining of the RECURSION

--- js/script.js
+++ js/script.js
@@ -3008,3 +3008,36 @@
 //     }
 //     console.log(node);
 // }
+
+
+//! lesson # 47: Recursion - The act of a function calling itself
+
+//todo normal way of the function
+// function givePower(x, n) {
+//   let result = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     result *= x;
+//   }
+//   return result;
+// }
+
+// console.log(givePower(2, 0)); //1
+// console.log(givePower(2, 1)); //2
+// console.log(givePower(2, 2)); //4
+
+//todo NOW how it can be if we use RECURSION
+function pow(x, n) {
+  if (n === 1) {
+    return x;
+  } else {
+    return x * pow (x, n -1);
+  }
+}
+
+console.log(pow(2, 1)) //2
+console.log(pow(2, 2)) //4
+console.log(pow(2, 3)) //8
+console.log(pow(2, 4)) //16
+console.log(pow(3, 3)) //27
+console.log(pow(9, 3)) //729
