00d45e397a81da0f1738868c966e8eefc9cc6e2d
Azamat Zhamankulov <azamat.zhamankulov.kk@gmail.com>
2024-06-08 21:33:14 +0600

    Added Lesson 55: Symbol in object

--- js/script.js
+++ js/script.js
@@ -3401,3 +3401,92 @@
 // let userName;
 // let userKey;
 // console.log(userName ?? userKey ?? 'DefaultUser');
+
+
+//! Oprional operator (?.)
+//! Optional chaining operator (?.)
+// const box = document.querySelector('.box');
+// const block = document.querySelector('.block');
+
+// // console.log(block); // null
+// // console.log(block.textContent); // here will be error
+// // console.log(1 + 2); // it well not showup because of prevouse error
+
+// //todo how to avoid it: we can put if condition
+// // console.log(block); // null
+
+// // if (block) {
+// //     console.log(block.textContent); // here because of condition it will not work
+// // }
+
+// // console.log(1 + 2); // 3
+
+// //todo but it is not comfortable so in this case we can use optional operator
+// //!Optional chaining operator (?.)=> first asking left side is there any value or not
+// console.log(block); // null
+
+// console.log(block?.textContent); // undefined (but it works only for reading the element)
+
+// console.log(1 + 2); // 3
+
+// // block?.textContent = '123'; //ERROR => we can not write something on undefined
+
+// const userData = {
+//     name: 'John',
+//     age: null,
+//     say: function() {
+//         console.log('Hello optional chaining operator!')
+//     }
+// }
+
+// // console.log(userData.skils.js); // userData doesn't have such key
+
+// // if (userData && userData.skills && userData.skills.js) {
+// //     console.log(userData.skills.js); // here will not be ERROR but it is long
+// // }
+
+// //todo here is the use of Optional chaining operator (?.)
+// console.log(userData.skills?.js); // undefined
+
+// userData.say(); // Hello optional chaining operator!
+// userData.hey?.(); //
+
+
+// //! Lesson #54 Live collections and useful methods
+// const boxesQuery = document.querySelectorAll('.box');
+// const boxesGet = document.getElementsByClassName('box');
+
+// //! so boxesGet is live collections
+
+// console.log(boxesQuery); // NodeList(3) => узлы - with methods
+// console.log(boxesGet); // HTMLCollection(3) => Элементы - without mehtods
+// // console.log(document.body.children); // HTMLCollection(3) => Элементы - without mehtods
+
+// boxesQuery[0].remove();
+// boxesGet[0].remove();
+
+// for (let i = 0; i < 5; i++) {
+//     const div = document.createElement('div');
+//     div.classList.add('box');
+//     document.body.append(div);
+// }
+
+// console.log(boxesQuery); // NodeList(3) => still have all elements
+// console.log(boxesGet); // HTMLCollection(6) => we get 6 elements but without methods
+// console.log(Array.from(boxesGet)); // now it is not LIVE collection it is just array
+
+
+//! Lessnon #55 Type of data Symbol
+// let id = Symbol("id1");
+
+// const obj = {
+//   name: 'Test',
+//   [id]: 1,
+//   getId: function() {
+//     return this[id];
+//   }
+// }
+
+// console.log(obj);
+
+// for (let value in obj) console.log(value);
